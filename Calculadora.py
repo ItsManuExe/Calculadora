bc22054809c18100654d1a585365cc35715c3309
JeffreyOlivar <142506322+JeffreyOlivar@users.noreply.github.com>
2023-08-17 08:15:33 -0500

    Update Calculadora.py
    xd

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,4 @@
+from tkinter import *
 import math
 
 # Función para manejar el clic en los botones
@@ -8,7 +9,7 @@ def click(event):x
     # Manejar las operaciones según el texto del botón
     if text == "=":
         try:
-            expression = screen.get().replace("^", "**")  # Reemplazar ^ con **
+            expression = screen.get().replace("^", "**")
             result = str(eval(expression))
             screen.set(result)
         except:
@@ -45,23 +46,23 @@ ourframe.config(cursor="hand2")
 
 # Definir la disposición de los botones y sus propiedades
 buttons = [
-    ("7", 2, 1, "#9CA7A5"), ("8", 2, 2, "#9CA7A5"), ("9", 2, 3, "#9CA7A5"), ("/", 5, 4, "#676767"),
-    ("4", 3, 1, "#9CA7A5"), ("5", 3, 2, "#9CA7A5"), ("6", 3, 3, "#9CA7A5"), ("*", 2, 4, "#676767"),
-    ("1", 4, 1, "#9CA7A5"), ("2", 4, 2, "#9CA7A5"), ("3", 4, 3, "#9CA7A5"), ("-", 3, 4, "#676767"),
-    ("0", 5, 2, "#9CA7A5"), (".", 5, 3, "#676767"), ("C", 1, 1, "#FF4500"), ("+", 4, 4, "#676767"),
-    ("=", 5, 1, "#D0E02A"), ("√", 1, 2, "#676767"), ("^", 1, 3, "#676767"), ("←", 1, 4, "#FF4500")
+    ("7", 2, 1, "#4b494a"), ("8", 2, 2, "#4b494a"), ("9", 2, 3, "#4b494a"), ("/", 5, 4, "#909ca6"),
+    ("4", 3, 1, "#4b494a"), ("5", 3, 2, "#4b494a"), ("6", 3, 3, "#4b494a"), ("*", 2, 4, "#909ca6"),
+    ("1", 4, 1, "#4b494a"), ("2", 4, 2, "#4b494a"), ("3", 4, 3, "#4b494a"), ("-", 3, 4, "#909ca6"),
+    ("0", 5, 2, "#4b494a"), (".", 5, 3, "#909ca6"), ("C", 1, 1, "#ff5c05"), ("+", 4, 4, "#909ca6"),
+    ("=", 5, 1, "#ff5c05"), ("√", 1, 2, "#909ca6"), ("^", 1, 3, "#909ca6"), ("←", 1, 4, "#FF4500")
 ]
 
-# Crear una variable para almacenar el contenido de la pantalla
+# crea el almacenamiento de la pantalla
 screen = StringVar()
 
 # Muestra el contenido de la pantalla
-entry = Entry(JAOC, textvar=screen, font="Arial 38", bd=10, insertwidth=1, width=14, justify="right", bg="#D0E02A")
+entry = Entry(JAOC, textvar=screen, font="Arial 38", bd=10, insertwidth=1, width=14, justify="right", bg="#cbcccd")
 entry.grid(row=0, column=1, columnspan=4)
 
 # Configura los botones y sus eventos de clic
 for (text, row, col, color, *span) in buttons:
-    button = Button(JAOC, text=text, font="Arial 15", padx=20, pady=20, bg=color, bd=8)
+  button = Button(JAOC, text=text, font="Arial 15", padx=20, pady=20, bg=color, fg="#FFFFFF", bd=5)
     rowspan = span[0] if span else 1
     columnspan = span[1] if len(span) > 1 else 1
     button.grid(row=row, column=col, rowspan=rowspan, columnspan=columnspan, sticky="nsew")
